73e9af19c382c823a504859bd9aae5ed37e8eb02
PierreP <paillard.pierre@gmail.com>
2021-04-25 20:37:53 +0200

    Sans service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularCoursHttpClient';
+export class AppComponent implements OnInit{
+  title = 'Pokemon List ! ';
+
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
 }
