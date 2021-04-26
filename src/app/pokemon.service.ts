21607194f2ec73b3a493593a4767fc93ee90f5d8
PierreP <paillard.pierre@gmail.com>
2021-04-26 15:10:24 +0200

    Service opé

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService{
 
+  private url: string = "https://api.pokemontcg.io"
+
+  constructor(private http : HttpClient){}
+
+  getPokemons(): Observable<any> {
+    return this.http.get(this.url+"/v1/cards")
+  }
 
 }
