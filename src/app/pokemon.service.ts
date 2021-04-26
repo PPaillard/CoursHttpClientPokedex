0bf84b6cf3a286e366f388068a9c83244f179994
PierreP <paillard.pierre@gmail.com>
2021-04-26 15:12:17 +0200

    j'avais oublié les commentaires

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -6,11 +6,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PokemonService{
-
+  // Url de base de l'API
   private url: string = "https://api.pokemontcg.io"
 
+  // j'instancie le client HTTP pour faire la requête sur l'API
   constructor(private http : HttpClient){}
 
+  // je vais chercher sur l'API la liste des pokemons.
   getPokemons(): Observable<any> {
     return this.http.get(this.url+"/v1/cards")
   }
